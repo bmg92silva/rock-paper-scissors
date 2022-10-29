b8208c96043cefb4088876d80453cdede66b2d47
bmg92silva <bruno92silva@outlook.pt>
2022-10-29 19:43:26 +0100

    Jogo com 5 rondas em consola

--- script.js
+++ script.js
@@ -5,8 +5,31 @@ console.log(playRound("Rock","Paper"));
 console.log(playRound("Paper","Rock"));
 */
 
-let player = prompt("Chose Rock, Paper or Scissor");
-console.log(playRound(player, getComputerChoice()));
+game();
+
+function game()  {
+  let userScore = 0;
+  let botScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    alert(`Round ${i+1}:`);
+    let playerChoice = prompt("Chose Rock, Paper or Scissor");
+    let botChoice = getComputerChoice();
+    console.log(`Player: ${playerChoice}`)
+    console.log(`Bot: ${botChoice}`)
+    let result = playRound(playerChoice, botChoice);
+    console.log(`Result: ${result}`)
+    console.log("-------------------------")
+    if (result === "You Win!") {
+      userScore++;
+    } else if (result === "You Lose!") {
+      botScore++;
+    }
+  }
+  console.log("Final Result:");
+  console.log(`Player: ${userScore}`);
+  console.log(`Bot: ${botScore}`);
+}
 
 function getComputerChoice() {
   let bot = Math.round(Math.random() * (3 - 1)) + 1;
